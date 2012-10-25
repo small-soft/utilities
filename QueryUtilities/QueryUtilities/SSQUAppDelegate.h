bcf326d857013bdbd4d12b9c9ed00d631ab679f9
small-soft <small-soft@live.com>
2012-10-25 14:06:32 +0800

    create QU

---
+++ QueryUtilities/QueryUtilities/SSQUAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  SSQUAppDelegate.h
+//  QueryUtilities
+//
+//  Created by 佳 刘 on 12-10-25.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class SSQUViewController;
+
+@interface SSQUAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) SSQUViewController *viewController;
+
+@end
---
+++ QueryUtilities/QueryUtilities/SSQUViewController.m
@@ -0,0 +1,34 @@
+//
+//  SSQUViewController.m
+//  QueryUtilities
+//
+//  Created by 佳 刘 on 12-10-25.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "SSQUViewController.h"
+
+@interface SSQUViewController ()
+
+@end
+
+@implementation SSQUViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
